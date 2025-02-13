b68960e2a39ade0efdf8e18bd36c93aeafc1b80f
yesolee <yesolee@kakao.com>
2025-02-13 21:11:29 +0900

    Fix: [기능] #149 카페 에이전트 - 에이전트 로직 및 반환 형식 수정, tool 검색 쿼리 변경

--- app/routers/agents/cafe_agent_router.py
+++ app/routers/agents/cafe_agent_router.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter, HTTPException
 from app.services.agents.cafe_agent_service import CafeAgentService
 from app.routers.agents.travel_all_schedule_agent_router import TravelPlanRequest
@@ -7,33 +8,24 @@ router = APIRouter()
 cafe_service = CafeAgentService()
 
 @router.post("/cafe")
-async def get_cafes(user_input: TravelPlanRequest):
+async def get_cafes(user_input: TravelPlanRequest, prompt:Optional[str]):
     """
     카페 정보를 가져오는 엔드포인트.
     - CrewAI 실행 후 일정(JSON) 반환.
     """
     try:
-        start_time = datetime.now()   
-
-        result = await cafe_service.create_recommendation(user_input.model_dump())
-        
-        end_time = datetime.now()
-        execution_time = (end_time - start_time).total_seconds()
-        
+        result = await cafe_service.create_recommendation(user_input.model_dump(),prompt=prompt)     
         if not result:
-            print("결과값이 없습니다. 실행 시간: {execution_time:.4f}초")
+            print("카페 결과값이 없습니다. ")
 
             raise HTTPException(
                 status_code=404,
                 detail={
                     "status": "error",
                     "message": "카페 검색 결과가 없습니다.",
-                    "execution_time": execution_time
                 }
             )    
 
-        print(f"`create_recommendation()` 실행 시간: {execution_time:.4f}초")
-        
         return {
             "status": "success",
             "message": "카페 리스트가 생성되었습니다.",
