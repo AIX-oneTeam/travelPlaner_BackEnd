4976d4e2284c943ac2b50e482518852d5a94e3f6
Jomalang <rhkddlr98@naver.com>
2025-02-16 00:11:05 +0900

    Add: [기능] fcm리포지 수정

--- app/repository/fcmToken/fcm_token_respository.py
+++ app/repository/fcmToken/fcm_token_respository.py
@@ -1,19 +1,19 @@
 from sqlmodel import select
-from app.data_models.data_model import FcmToken
+from app.data_models.data_model import MessageToken
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 async def save_fcm_token(member_id: int, fcm_token: str, session: AsyncSession):
     print("💡[ fcm_token_respository ] save_fcm_token() : ", member_id, fcm_token)
     try:
-        session.add(FcmToken(member_id=member_id, fcm_token=fcm_token))
+        session.add(MessageToken(member_id=member_id, token=fcm_token))
     except Exception as e:
         print("💡[ fcm_token_respository ] save_fcm_token() 에러 : ", e)
 
 async def get_fcm_token(member_id: int, session: AsyncSession):
     print("💡[ fcm_token_respository ] get_fcm_token() : ", member_id)
     try:
-        result = await session.exec(select(FcmToken).where(FcmToken.member_id == member_id))
+        result = await session.exec(select(MessageToken).where(MessageToken.member_id == member_id))
         fcm_token = result.first()
         return fcm_token
     except Exception as e:
