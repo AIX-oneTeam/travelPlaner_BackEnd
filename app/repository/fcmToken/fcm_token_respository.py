e02f4d279ac7ee29f5b952e1b94ae0379d80fec7
Jomalang <rhkddlr98@naver.com>
2025-02-18 01:27:34 +0900

    Fix: [기능] fcm토큰 저장 로직 수정

--- app/repository/fcmToken/fcm_token_respository.py
+++ app/repository/fcmToken/fcm_token_respository.py
@@ -4,6 +4,7 @@ from app.data_models.data_model import MessageToken
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 async def get_fcm_token(member_id: int, session: AsyncSession):
     logger.info("💡[ fcm_token_respository ] get_fcm_token() : ", member_id)
@@ -26,15 +27,16 @@ async def save_fcm_token(member_id: int, fcm_token: str, session: AsyncSession):
             session.add(MessageToken(member_id=member_id, token=fcm_token))
             logger.info("💡[ fcm_token_respository ] save_fcm_token() 새 fcm토큰을 저장합니다. : ", member_id, fcm_token)
         # 존재하되 같으면 무시
-        elif existing_token.token == fcm_token:
+        elif existing_token == fcm_token:
             logger.info("💡[ fcm_token_respository ] save_fcm_token() 같은 fcm토큰이 이미 존재합니다. : ", member_id, fcm_token)
             return
         # 존재하되 다르면 업데이트
         else:
             logger.info("💡[ fcm_token_respository ] save_fcm_token() fcm 토큰 업데이트 : ", member_id, fcm_token)
             # 토큰 업데이트
-            existing_token.token = fcm_token
-            session.add(existing_token)
+            query = update(MessageToken).where(MessageToken.member_id == member_id).values(token=fcm_token)
+            await session.exec(query)
+            await session.commit()
         
     except Exception as e:
         logger.error("💡[ fcm_token_respository ] save_fcm_token() 에러 : ", e)
