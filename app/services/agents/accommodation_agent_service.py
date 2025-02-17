30103b53564942e053e4c669e700969ae3656148
eun <eun00864@gmail.com>
2025-02-17 19:05:42 +0900

    Fix:[수정] 숙소 에이전트 세분화

--- app/services/agents/accommodation_agent_service.py
+++ app/services/agents/accommodation_agent_service.py
@@ -13,11 +13,14 @@ import json
 import http.client
 from app.services.agents.accommodation_tools import GeoCoordinateTool, GoogleMapTool, GoogleReviewTool, GoogleHotelSearchTool
 from app.dtos.spot_models import spots_pydantic
+import logging
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 SERP_API_KEY = os.getenv("SERP_API_KEY")
+
+logger = logging.getLogger(__name__)
 
 class AccommodationAgentService:
     _instance = None
@@ -44,6 +47,62 @@ class AccommodationAgentService:
         self.google_hotel_search_tool = GoogleHotelSearchTool()
         self.gooloe_review_tool = GoogleReviewTool()
         self.agents = self.create_accommodation_agnets()
+        
+    def prepare_crew_inputs(self, user_input: dict):
+        """크루에 전달될 데이터 전처리"""
+        
+        companion_group = user_input.get('companion_count', [])
+
+        # 동반자 정보를 label을 기준으로 합산하기 위한 dictionary 생성
+        age_groups = {
+            "성인": 0,
+            "청소년": 0,
+            "어린이": 0,
+            "영유아": 0,
+            "반려견": 0,
+        }
+
+        # companion_group 리스트를 순회하며 각 label에 해당하는 count 값을 합산
+        for companion in companion_group:
+            label = companion.get("label")
+            count = companion.get("count", 0)
+            if label in age_groups:
+                age_groups[label] += count
+
+        # 성인과 청소년을 합쳐서 adults로, 어린이와 영유아를 합쳐서 children으로 계산
+        adults = age_groups["성인"] + age_groups["청소년"]
+        children = age_groups["어린이"] + age_groups["영유아"]
+        pets = age_groups["반려견"]
+
+        # keyword 추출
+        concepts = user_input.get('concepts', [])
+        user_prompt = user_input.get("prompt")
+        keywords = concepts + ([user_prompt] if user_prompt else [])
+        keywords = ', '.join(filter(None, keywords))  
+        
+        # age 추출 및 키워드에 추가
+        age = user_input.get('ages')
+        if age:
+            keywords.append(age)
+        
+        # 반려견 동반 여부 키워드 추가
+        if pets > 0:
+            keywords.append("반려견 동반")
+        else:
+            keywords.append("반려견 미동반")
+
+        prepared_user_data = {
+            'main_location' : user_input.get('main_location'),
+            'ages': user_input.get('ages'),
+            'start_date': user_input.get('start_date'),
+            'end_date': user_input.get('end_date'),
+            'adults': adults,
+            'children': children,
+            'pets': pets,
+            'keywords': keywords
+        }
+        
+        return prepared_user_data
 
     def create_accommodation_agnets(self):
         """에이전트 생성 메서드"""
@@ -92,6 +151,14 @@ class AccommodationAgentService:
                 verbose=True,
                 async_execution=True,
             ),
+            "accommodation_user_prompt_keyword_expert": Agent(
+                role="사용자 키워드 추출 전문가",
+                goal="사용자가 입력한 input에서 키워드를 추출한다.",
+                backstory="나는 키워드 추출 전문가로, llm을 사용하여 사용자가 입력한 input에서 키워드를 추출합니다.",
+                llm=self.llm,
+                verbose=True,
+                async_execution=True,
+            ),            
             "accommodation_keyword_compare_expert": Agent(
                 role="사용자 입력 키워드와 추출 키워드 비교 전문가",
                 goal="사용자가 입력한 키워드 값과 숙소에서 추출한 키워드를 비교하여 사용자 숙소 추천 리스트를 만든다",
@@ -100,16 +167,8 @@ class AccommodationAgentService:
                 verbose=True,
                 async_execution=True,
             ),
-            "accommodation_user_prompt_keyword_expert": Agent(
-                role="사용자 키워드 추출 전문가",
-                goal="사용자가 입력한 input에서 키워드를 추출한다.",
-                backstory="나는 키워드 추출 전문가로, llm을 사용하여 사용자가 입력한 input과 프롬프트에서 키워드를 추출합니다.",
-                llm=self.llm,
-                verbose=True,
-                async_execution=True,
-            ),            
         }
-    def create_accommodation_tasks(self, input_data : dict, promt_test :str):
+    def create_accommodation_tasks(self, input_data : dict):
         """테스크 생성 메서드"""
         return[
             Task( #좌표 task
@@ -146,7 +205,7 @@ class AccommodationAgentService:
                 -1번 키워드는 반드시 해당 숙소의 타입을 나타내는 키워드를 추출합니다. 예) 호텔, 리조트, 빌라, 게스트하우스, 에어비엔비
                 -2번 키워드는 반드시 해당 숙소의 추천 연령대를 포함합니다 예)10대, 20대, 30대, 40대, 50대, 60대,70대,80대
                 -3번 키워드는 반드시 추천 단체를 포함합니다 예)가족, 친구, 연인, 혼자
-                -4번 키워드는 반드시 반려견 동반 가능 여부를 포함합니다 예)반려견 가능, 반려견 불가능
+                -4번 키워드는 반드시 반려견 동반 가능 여부를 포함합니다 예)반려견 동반, 반려견 미동반
                 -5번 키워드는 반드시 해당 숙소에 있는 부대 시설을 포함합니다. 예) 헬스장, 수영장, 바, 어린이 놀이 시설, 만약 이 중 제공하는 시설이 없다면 공용 와이파이로 나타냅니다.
                 -6번 키워드는 반드시 해당 숙소의 친절도를 나타냅니다 예)매우 친절, 적당히 친절, 보통, 불친절, 매우 불친절
                 -7번 키워드는 반드시 해당 숙소의 접근성에 대해 나타냅니다. 예) 접근성 매우 좋음, 접근성 약간 좋음, 접근성 보통, 접근성 약간 불편, 접근성 매우 불편
@@ -154,20 +213,19 @@ class AccommodationAgentService:
                 -9번 키워드는 반드시 해당 숙소의 뷰에 대해서 나타냅니다. 예)오션뷰, 산뷰, 빌딩뷰, 주차장뷰, 뷰없음 
                 -10번 키워드는 반드시 가까운 장소에 대해서 언급합니다. 리뷰에서 가깝다고 언급한 장소를 키워드로 합니다.
                 """,
-                agent=self.agents["geocoding_expert"],
+                agent=self.agents["accommodation_review_expert"],
                 expected_output="숙소 리뷰 기반 추출 키워드 10개",
             ), 
             Task( #프롬프트 키워드 추출 task
                 description=f"""
-                - 사용자가 입력한 {input_data['user_prompt']}가 없다면 {input_data['user_input']}에서만 키워드를 추출합니다.
-                - 사용자가 입력한 {input_data['user_prompt']}가 있다면 {input_data['user_input']}와{input_data['user_prompt']}에서 키워드를 추출합니다.""",
-                agent=self.agents["geocoding_expert"],
+                - 사용자가 입력한 {input_data['keywords']}에서 키워드를 추출합니다.""",
+                agent=self.agents["accommodation_user_prompt_keyword_expert"],
                 expected_output="키워드",
             ),             
             Task( #사용자 키워드와 리뷰 키워드 비교 task
                 description=f"""
                 - 사용자가 입력에서 추출한 키워드와 숙소 리뷰 기반 추출 키워드를 비교하여 더 많은 키워드가 일치하는 숙소의 기본 정보를 상위로 위치하게 나열하여 숙소 추천 리스트를 제공합니다.""",
-                agent=self.agents["accommodation_user_prompt_keyword_expert"],
+                agent=self.agents["accommodation_keyword_compare_expert"],
                 expected_output="""
                 - 일치하는 키워드가 많은 숙소가 상위 위치한 숙소 리스트
                 - spot_category: 0 으로 항상 고정
@@ -176,58 +234,26 @@ class AccommodationAgentService:
             ),                       
         ]
 
-    async def accommodation_agent(self, user_input: dict):
+    async def create_accommodation_recemmendation(self, user_input: dict):
         """
         CrewAI를 실행하여 사용자 맞춤 숙소를 추천하는 서비스
         """
-        if user_input is None:
-            raise ValueError("user_input이 없습니다. 잘못된 요청을 보냈는지 확인해주세요")
-
-        ages = user_input.get('ages')
-        start_date = user_input.get('start_date')
-        end_date = user_input.get('end_date')
-        companion_group = user_input.get('companion_count', [])
-
-        # 동반자 정보를 label을 기준으로 합산하기 위한 dictionary 생성
-        age_groups = {
-            "성인": 0,
-            "청소년": 0,
-            "어린이": 0,
-            "영유아": 0,
-            "반려견": 0,
-        }
-
-        # companion_group 리스트를 순회하며 각 label에 해당하는 count 값을 합산
-        for companion in companion_group:
-            label = companion.get("label")
-            count = companion.get("count", 0)
-            if label in age_groups:
-                age_groups[label] += count
-
-        # 성인과 청소년을 합쳐서 adults로, 어린이와 영유아를 합쳐서 children으로 계산
-        adults = age_groups["성인"] + age_groups["청소년"]
-        children = age_groups["어린이"] + age_groups["영유아"]
-        pets = age_groups["반려견"]
-
-        user_input["concepts"] = ', '.join(user_input.get('concepts', ''))
-        user_input["user_prompt"] = user_input.get("prompt")  # 오타 수정
-
-        # 멀티 에이전트 시스템 설정
-        crew = Crew(
-            agents=[self.accommodation_recommendation_expert],
-            tasks=[self.accommodation_recommendation_task],
-            process=Process.sequential,
-            verbose=True,
-        )
-
-        # 실행
+        
         try:
-            result = await crew.kickoff_async(inputs=user_input)
-            print(result)
-            return result.json_dict.get("spots", [])
-        except Exception as e:
-            print(f"Error during execution: {e}")
+            # 1. 입력 데이터 전처리
+            processed_input= self.prepare_crew_inputs(user_input)
 
-            # 오류가 발생한 경우 Observation을 직접 확인
-            if hasattr(e, 'Observation'):
-                print(f"Tool Output (Observation): {e.Observation}")
+            # 2. Task 생성
+            tasks = self.create_accommodation_tasks(processed_input)
+
+            # 3. Crew 실행
+            crew = Crew(tasks=tasks, agents=list(self.agents.values()), verbose=True, memory=True, process=Process.sequential)
+
+            # 4. 결과 처리
+            result = await crew.kickoff_async()
+            return result.json_dict.get("spots", [])
+
+        except Exception as e:
+            print(f"[accommodation agent error] --- accommodation agent error {e}")
+            logger.error(f"[accommodation agent error] --- accommodation agent error {e}")
+            
