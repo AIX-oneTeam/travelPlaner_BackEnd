5af21659e0bf7359cf69df6019edb2712290affd
gomting0 <zlstm7@gmail.com>
2025-02-25 15:19:18 +0900

    [기능] 멤버 오퍼레이션  구현

--- app/services/members/member_service.py
+++ app/services/members/member_service.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import Request
-from app.repository.members.mebmer_repository import get_memberId_by_email
+from collections import Counter
+from app.repository.members.mebmer_repository import get_memberId_by_email,get_member_signup_count
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 logger = logging.getLogger(__name__)
@@ -19,4 +20,30 @@ async def get_member_id_by_request(request: Request, session: AsyncSession):
             return None
     except Exception as e:
         logger.error(f"💡[ member_service ] get_member_id_by_request() 에러 : {e}")
+
+
+
+async def get_member_signup_count_service(session: AsyncSession):
+    
+    # 리포지토리 함수 호풀
+    crate_dates = await get_member_signup_count(session)
+
+    #날짜를 문자열로 변환
+    date_string= [
+        dt.strftime("%Y-%m-%d")
+        for dt in crate_dates  if dt is not None
+    ]
+    # 나온 결과를 집계
+    counts = Counter(date_string)
+
+    #
+    signup_counts = [
+        {"signup_date": date, "member_count": count}
+        for date , count in counts.items()
+    ]
+    
+    signup_counts.sort(key=lambda x: x["signup_date"], reverse=True)
+
+    return signup_counts
+        
 
